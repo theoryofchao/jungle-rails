78a62b7b5437a4d6a3a33eef07f5b9316da3b396
theoryofchao <roger.ti.chao@gmail.com>
2017-02-16 19:41:31 -0500

    step 2 validation for ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ActiveRecord::Base
+
+  belongs_to :product
+  belongs_to :user
+
+  validates :product_id, :user_id, :rating, presence: true
+  validates :product_id, uniqueness: { scope: :user_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
+  has_many :reviews
   has_secure_password
 
 end
