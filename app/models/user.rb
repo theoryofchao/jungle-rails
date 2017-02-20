80bca53cd917078d589b04189b0fb5d03ff7ca47
theoryofchao <roger.ti.chao@gmail.com>
2017-02-20 14:02:05 -0500

    step 2 validation specs users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
+  before_validation :downcase_stuff
+
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
   has_many :reviews
   has_secure_password
+
+  private
+    def downcase_stuff
+      if (self.email)
+        self.email.downcase!
+      end
+    end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,49 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    @user = Product.new
+    @user = User.new
+  end
+
+  it "should have matching passwords" do
+    user = User.new(first_name: 'first_name', last_name: 'last_name', email: "first@last.com", password: 'password', password_confirmation: 'password')
+    expect(user.save).to be_truthy
+  end
+
+  it "should not have mismatched passwords" do
+    user = User.new(first_name: 'first_name', last_name: 'last_name', email: "first@last.com", password: 'right', password_confirmation: 'wrong')
+    expect(user.save).to be_falsey
+  end
+
+  it "should not have accounts with same email" do
+    user = User.create(first_name: 'first_name', last_name: 'last_name', email: "first@last.com", password: 'password', password_confirmation: 'password')
+    user2 = User.new(first_name: 'first_name', last_name: 'last_name', email: "first@last.com", password: 'password', password_confirmation: 'password')
+    expect(user2.save).to be_falsey
+  end
+
+  it "should not matter about email case" do
+    user = User.create(first_name: 'first_name', last_name: 'last_name', email: "first@last.com", password: 'password', password_confirmation: 'password')
+    user2 = User.new(first_name: 'first_name', last_name: 'last_name', email: "FIRST@LAST.com", password: 'password', password_confirmation: 'password')
+    expect(user2.save).to be_falsey
+  end
+
+  it "should have email first_name last_name 1" do
+    user = User.new(first_name: 'firstname', last_name: nil, email: nil, password: 'password', password_confirmation: 'password')
+    expect(user.save).to be_falsey
+  end
+
+  it "should have email first_name last_name 2" do
+    user = User.new(first_name: 'firstname', last_name: 'lastname', email: nil, password: 'password', password_confirmation: 'password')
+    expect(user.save).to be_falsey
+  end
+
+  it "should have email first_name last_name 3" do
+    user = User.new(first_name: 'firstname', last_name: 'last_name', email: "first@last.com", password: 'password', password_confirmation: 'password')
+    expect(user.save).to be_truthy
   end
 end
+
+
+# validates :first_name, :last_name, :email, :password_digest, presence: true
+# validates :email, uniqueness: true
+# has_many :reviews
+# has_secure_password
